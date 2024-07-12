f9bfa040d5d8b563aca249646ebf75df8cae28fa
MOONJAMIN <wkals0801@naver.com>
2024-07-12 17:37:59 +0900

    movie_app

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MWEzZjY5MzU5Y2QzZGZiNDEwZTA5MGQ0NWEzNzY5NSIsIm5iZiI6MTcyMDc2ODAxOC42OTUwMDYsInN1YiI6IjY2OGY5NzUxOGUzOWM5YzlmMjEwZWUyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZPAGpIaHpOTrIZdyfqGGu3EfkY7ZXqL4iMHc3G_RwMs",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?language=ko-kr`;
+};
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
