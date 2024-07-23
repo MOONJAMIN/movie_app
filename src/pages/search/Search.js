82a54c12f21c9d684dbb243a197f8e0dae422dc4
MOONJAMIN <wkals0801@naver.com>
2024-07-23 17:35:09 +0900

    movie_app

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -2,8 +2,11 @@ import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { IoMdSearch } from "react-icons/io";
 import { spacing } from "../../GlobalStyled";
-import { useEffect } from "react";
+import { useState } from "react";
 import { movieSearch } from "../../api";
+import { Link } from "react-router-dom";
+import { Loading } from "../../components/Loading";
+import { W500_URL } from "../../constant/imgUrl";
 
 const Container = styled.div`
   padding: 150px ${spacing.side};
@@ -39,7 +42,27 @@ const ErrorMessage = styled.h4`
   margin-top: 20px;
 `;
 
+const ConWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* =>mask-repeat(반복수량, 이미지크기) */
+  /* 1fr = 전체가로 비율에서 똑같은 사이즈로 보여주고 싶을 때 사용함 */
+  row-gap: 30px;
+  column-gap: 15px;
+`;
+const Con = styled.div``;
+
+const Bg = styled.div`
+  height: 500px;
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
 export const Search = () => {
+  const [searchData, setSearchData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const {
     register,
     handleSubmit,
@@ -48,9 +71,16 @@ export const Search = () => {
 
   const onSearchResult = async (data) => {
     const { keyword } = data;
-    const result = await movieSearch(keyword);
-    console.log(result);
+
+    const { results } = await movieSearch(keyword);
+
+    setSearchData(results);
+    setIsLoading(false);
   };
+
+  console.log(searchData?.length === 0 ? "없음" : "있음");
+
+  // const {results} = await searchMovie(keyword);
 
   return (
     <Container>
@@ -67,6 +97,35 @@ export const Search = () => {
         </button>
         <ErrorMessage>{errors?.keyword?.message}</ErrorMessage>
       </Form>
+
+      {searchData?.length === 0 ? (
+        "검색결과없음"
+      ) : (
+        <>
+          {searchData && (
+            <ConWrap>
+              {isLoading ? (
+                <Loading />
+              ) : (
+                <>
+                  {searchData.map((data) => (
+                    <Con key={data.id}>
+                      <Link to={`/detail/${data.id}`}>
+                        <Bg>
+                          <img
+                            src={W500_URL + data.poster_path}
+                            alt={data.title}
+                          />
+                        </Bg>
+                      </Link>
+                    </Con>
+                  ))}
+                </>
+              )}
+            </ConWrap>
+          )}
+        </>
+      )}
     </Container>
   );
 };
